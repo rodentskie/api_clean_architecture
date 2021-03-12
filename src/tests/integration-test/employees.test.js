1c3b13ae6f64b6510b6fc819d7aefea85a93328e
rodentskie <rodentskie@gmail.com>
2021-03-12 14:26:48 +0800

    done integ test update

--- src/tests/integration-test/employees.test.js
+++ src/tests/integration-test/employees.test.js
@@ -9,112 +9,88 @@ const routes = require("./employees"); // routes to be tested
 
 describe(`Employees Tests Suites`, () => {
   test(`Select Employees`, async () => {
-    try {
-      const res = await routes.selectEmployees();
-      const data = res.status;
-      expect(data).toBe(200);
-    } catch (e) {
-      console.log("Error: ", e);
-    }
+    const res = await routes.selectEmployees();
+    const data = res.status;
+    expect(data).toBe(200);
   });
 
   test(`Add Employees - All fields have value.`, async () => {
-    try {
-      const info = {
-        firstName: randomstring.generate({
-          length: 12,
-          charset: "alphabetic",
-        }), // generate random string
-        lastName: randomstring.generate({
-          length: 12,
-          charset: "alphabetic",
-        }), // generate random string
-        age: 13,
-      };
-      const res = await routes.addEmployees({ info });
-      const data = res.status;
-      expect(data).toBe(201);
-    } catch (e) {
-      console.log(`Error: `, e);
-    }
+    const info = {
+      firstName: randomstring.generate({
+        length: 12,
+        charset: "alphabetic",
+      }), // generate random string
+      lastName: randomstring.generate({
+        length: 12,
+        charset: "alphabetic",
+      }), // generate random string
+      age: 13,
+    };
+    const res = await routes.addEmployees({ info });
+    const data = res.status;
+    expect(data).toBe(201);
   });
 
   test(`Add Employees - Required fields missing.`, async () => {
-    try {
-      const info = {
-        firstName: randomstring.generate({
-          length: 12,
-          charset: "alphabetic",
-        }), // generate random string
-        lastName: null,
-        age: 8,
-      };
-      const res = await routes.addEmployees({ info });
-      const data = res.response.status;
-      expect(data).toBe(400);
-    } catch (e) {
-      console.log(`Error: `, e);
-    }
+    const info = {
+      firstName: randomstring.generate({
+        length: 12,
+        charset: "alphabetic",
+      }), // generate random string
+      lastName: null,
+      age: 8,
+    };
+    const res = await routes.addEmployees({ info });
+    const data = res.response.status;
+    expect(data).toBe(400);
   });
 
   test(`Update Employees - All fields have value.`, async () => {
-    try {
-      const emp = await routes.selectEmployees();
-      const employees = emp.data.view;
-      const id = employees[employees.length - 1].id;
-      const info = {
-        firstName: randomstring.generate({
-          length: 12,
-          charset: "alphabetic",
-        }), // generate random string
-        lastName: randomstring.generate({
-          length: 12,
-          charset: "alphabetic",
-        }), // generate random string
-        age: 9,
-      };
+    const emp = await routes.selectEmployees();
+    const employees = emp.data.view;
+    const id = employees[employees.length - 1].id;
+    const info = {
+      firstName: randomstring.generate({
+        length: 12,
+        charset: "alphabetic",
+      }), // generate random string
+      lastName: randomstring.generate({
+        length: 12,
+        charset: "alphabetic",
+      }), // generate random string
+      age: 9,
+    };
 
-      const res = await routes.updateEmployees({ id, info });
-      const data = res.status;
-      expect(data).toBe(200);
-    } catch (e) {
-      console.log("Error: ", e);
-    }
+    const res = await routes.updateEmployees({ id, info });
+    const data = res.status;
+    expect(data).toBe(200);
   });
 
   test(`Update Employees - Required fields are missing.`, async () => {
-    try {
-      const emp = await routes.selectEmployees();
-      const employees = emp.data.view;
-      const id = employees[employees.length - 1].id;
-      const info = {
-        firstName: null,
-        lastName: randomstring.generate({
-          length: 12,
-          charset: "alphabetic",
-        }), // generate random string
-        age: 9,
-      };
+    const emp = await routes.selectEmployees();
+    const employees = emp.data.view;
+    const id = employees[employees.length - 1].id;
+    const info = {
+      firstName: null,
+      lastName: randomstring.generate({
+        length: 12,
+        charset: "alphabetic",
+      }), // generate random string
+      age: 9,
+    };
 
-      const res = await routes.updateEmployees({ id, info });
-      const data = res.response.status;
-      expect(data).toBe(400);
-    } catch (e) {
-      console.log("Error: ", e);
-    }
+    const res = await routes.updateEmployees({ id, info });
+    const data = res.response.status;
+    expect(data).toBe(400);
   });
 
   test(`Delete Employees`, async () => {
-    try {
-      const emp = await routes.selectEmployees();
-      const employees = emp.data.view;
-      const id = employees[employees.length - 1].id;
+    const emp = await routes.selectEmployees();
+    const employees = emp.data.view;
+    const id = employees[employees.length - 1].id;
 
-      const res = await routes.deleteEmployees({ id });
-      const data = res.status;
-      expect(data).toBe(200);
-    } catch (e) {
-      console.log("Error: ", e);
-    }
+    const res = await routes.deleteEmployees({ id });
+    const data = res.status;
+    expect(data).toBe(201);
   });
 });
