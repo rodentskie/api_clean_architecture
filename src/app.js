6eb7c8b6a515d4abd3d279525c9dfb0c9c2f172c
rodentskie <rodentskie@gmail.com>
2021-05-19 06:38:07 +0800

    wee

--- src/app.js
+++ src/app.js
@@ -26,6 +26,6 @@ app.use(async (req, res) => {
   res.status(404).send(`Route is no where to be found.`);
 });
 
-console.log(`testing cicd`);
+console.log(`testing cicd 2nd test`);
 
 module.exports = app;
