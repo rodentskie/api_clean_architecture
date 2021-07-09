50d5153cb6cd67e55714abfe02cdcb9f8388b6f6
rodentskie <rodentskie@gmail.com>
2021-07-09 21:22:12 +0800

    dev

--- src/app.js
+++ src/app.js
@@ -21,6 +21,8 @@ const server = app.listen(PORT, () => {
 // routes
 app.use("/api/employees", require("./routes/employees/app"));
 
+console.log('test');
+
 // when invalid routes are entered
 app.use(async (req, res) => {
   res.status(404).send(`Route is no where to be found.`);
